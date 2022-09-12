4573ed30e868818a24a01cf2e5636826afecebe9
Arbusta <gabriel.munoz@arbusta.net>
2022-09-12 20:31:56 -0300

    Clase 12-9

---
+++ Codigo/7 12-09/app.js
@@ -0,0 +1,95 @@
+
+//array - matriz - arreglo
+                     //indice
+/* let instrumentos = ["piano","trompeta","bajo",1,5,true,false]; */
+            //         0        1        2    3 4  5    6
+
+
+            // scope
+//funciones
+/* function sumar(num1, num2) {
+    return num1+num2
+}
+sumar(5,6)
+ */
+
+//arrow - funcion flecha
+/* const sumar = (num1, num2)=>{
+    return num1+num2
+    }
+    sumar(5,6) */
+
+
+ //Callback?
+/* function name(params) {
+    
+}
+name() */
+
+//-----------------------------------
+// tarea
+/* operadores de comparacion y su funcionalidad
+&& and
+|| or
+!=
+!==
+ */
+
+
+let num1 =10;
+let num2 = 20;
+let num3 = "10";
+  // AND
+/* let resultado = (num1<num2)&&(num3===num1); */
+                //  true          false 
+let resultado = (num1>num2)||(num3==num1);
+                // false         true
+                       //true
+/* document.write(resultado) */
+
+
+// NOT !
+
+/* (`!==`)  no es igual" no es igual" */
+/* Esto devolverá verdadero si los artículos NO son iguales entre sí de alguna manera. */
+/* 1 !== 1;   */        // false
+/* 1 !== '1';  */       // true
+/* 'perro' !== 'perro'; */  // false
+/* 'perro' !== 'Perro'; */  // true
+
+//Flujos de control
+
+/* let edad = 40
+
+if (edad>=18) {
+document.write("Podes Pasar")
+} else {
+document.write ("NO Podes Pasar")
+} */
+
+//funciones
+
+/* function saludar (nombre,numero){
+document.write(`Hola ${nombre} tu numero es el ${numero}`)  
+}
+saludar  (prompt("decime tu nombre"), parseInt(prompt("decime tu edad"))) */
+
+/* function sumar (num1, num2){
+    document.write (num1+num2)
+}
+sumar (10,10)
+sumar (20,10)
+sumar (10,50)
+ */
+
+/* function saludar (){
+    document.write ("hola")
+}
+saludar ()
+saludar () */
+
+
+
+
+
+
