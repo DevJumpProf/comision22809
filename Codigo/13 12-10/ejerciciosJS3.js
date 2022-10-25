48ec219f9462ea1d462ba2937ac15cd86bfa40ee
Arbusta <gabriel.munoz@arbusta.net>
2022-10-25 01:06:13 -0300

    REACT

--- Codigo/13 12-10/ejerciciosJS3.js
+++ Codigo/13 12-10/ejerciciosJS3.js
@@ -184,6 +184,8 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+
+  
   
 } 
 
---
+++ Codigo/14 24-10/REACT/mi-primer-app/src/boton.jsx
@@ -0,0 +1,10 @@
+
+function Boton (){
+    return (
+    <button>
+      1
+        </button>
+    )
+}
+
+export default Boton;
