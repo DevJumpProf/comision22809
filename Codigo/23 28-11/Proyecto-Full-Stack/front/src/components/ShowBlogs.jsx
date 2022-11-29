5ec5d6d4f984263f858ec4ca5f0968d381318d3b
Arbusta <gabriel.munoz@arbusta.net>
2022-11-28 21:43:44 -0300

    proyecto terminado

---
+++ Codigo/23 28-11/Proyecto-Full-Stack/front/src/components/ShowBlogs.jsx
@@ -0,0 +1,77 @@
+import axios from "axios"
+import { useState,useEffect } from "react"
+import {Link} from "react-router-dom"
+
+const url = "http://localhost:8000/blogs/"
+
+const ShowBlogs = ()=>{
+
+    const [blogs,setBlog]= useState([])
+
+    useEffect(()=>{
+        getBlogs()
+    },[])
+
+//procedimiento para mostrar todos los blogs
+
+const getBlogs = async ()=>{
+    const res = await axios.get(url)
+   setBlog(res.data)
+}
+
+//procedimiento para eliminar un blog
+const deleteBlog = async (id)=>{
+await axios.delete(`${url}${id}`)
+getBlogs()
+}
+
+return(
+    <div className="container">
+<div className="row">
+    <div className="col">
+<small>CREAR BLOG</small>
+<Link to="/create" className="btn btn-primary mt-2 mb-2"><i className="fas fa-plus"></i></Link>
+
+<table className="table">
+    <thead className="table-primary">
+<tr>
+     <th>ID</th>
+    <th>Title</th>
+    <th>content</th>
+    <th>Actions</th>
+</tr>
+    </thead>
+
+
+<tbody>
+{blogs.map((blog)=>(
+<tr key={blog.id}>
+<td>{blog.id} </td>  
+<td>{blog.title} </td>  
+<td>{blog.content} </td>
+<td>
+    <Link to = {`/edit/${blog.id}`} className="btn btn-info"><i className="fas fa-edit"></i> </Link>
+    <button onClick={()=>deleteBlog(blog.id)} className="btn btn-danger"><i className="fas fa-trash"></i></button>
+</td>
+</tr>
+
+
+))}
+
+</tbody>
+
+</table>
+
+
+    </div>
+</div>
+
+
+    </div>
+)
+
+
+
+}
+
+export default ShowBlogs
